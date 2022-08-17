b2aeb37c8f66f8bb5359dbad9750e66163c29cf7
Mike Kelly <mike@membervault.co>
2022-08-16 19:00:28 -0700

    WIP install

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+
+sudo apt systemctl disable snapd
+sudo apt purge snapd
+
+sudo apt install sway xwayland dmenu foot arc-theme papirus-icon-theme gnome-icon-theme thunar slurp grim lxappearance neovim light
+
+sudo apt install telegram-desktop
+
+
+# Install VSCodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+
+sudo apt update && sudo apt install codium
+
+
+# Install Firefox ESR
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt update
+sudo apt install firefox-esr
+
+# Install Docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+
+
+# Clean Up
+sudo apt autoremove
